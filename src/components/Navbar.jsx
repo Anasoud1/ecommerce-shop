5b3d36cf715b79c8b833046cc2745139a2d1f5d0
anas <anasoudsassi@gmail.com>
2025-01-21 13:37:29 +0100

    add navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
 'use client'
+import { AlignRight, ChevronLeft } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
 
+    const [visible, setVisible] = useState(false)
     const path = usePathname()
-    console.log('path: ', path)
 
     return (
         <div className='flex items-center justify-between'>
-            <h1 className='font-bold text-xl'>Logo</h1>
-            <ul className='flex items-center gap-6 text-sm font-medium'>
+            <h1 className='font-bold text-xl'>eShop</h1>
+            <ul className='hidden sm:flex items-center gap-6 text-sm font-medium'>
                 <Link href={'/'} className='flex flex-col items-center'>
                     <li >HOME</li>
                     <hr className={`${path === '/' ? '' : 'hidden'} w-2/4 border-none h-[1px] bg-gray-700`} />
@@ -33,9 +34,11 @@ const Navbar = () => {
             </ul>
             <div className='flex items-center gap-4'>
                 <Image src={'/search.png'} width={20} height={20} alt='search' />
-                <Link href={'/cart'} className='cursor-pointer'>
+                <Link href={'/cart'} className='cursor-pointer relative'>
                     <Image src={'/cart.png'} width={20} height={20} alt='cart' />
+                    <p className='absolute bg-black text-white bottom-[-5px] right-[-5px] rounded-full w-4 text-center aspect-square leading-4 text-[8px]'>10</p>
                 </Link>
+
 
                 <div className='group relative cursor-pointer'>
                     <Image src={'/user.png'} width={20} height={20} alt='user' />
@@ -47,9 +50,28 @@ const Navbar = () => {
                         </ul>
                     </div>
                 </div>
+                <AlignRight onClick={() => setVisible(true)} className='block sm:hidden cursor-pointer' />
+
 
 
             </div>
+
+            {/* ------- SideBar ------- */}
+            <ul className={`${visible ? 'w-full': 'w-0'} absolute top-0 right-0 bottom-0 overflow-hidden bg-white flex flex-col`}>
+                <li  className='flex py-3 gap-x-3 pl-4'> <ChevronLeft onClick={() => setVisible(false)} className='cursor-pointer' size={26}/> Back</li>
+                <Link onClick={() => setVisible(false)}  href={'/'} className={`${path === '/' ? 'active' : ''} border w-full py-2 pl-8`}>
+                    <li >HOME</li>
+                </Link>
+                <Link onClick={() => setVisible(false)}  href={'/collection'} className={`${path === '/collection' ? 'active' : ''} border w-full py-2 pl-8`}>
+                    <li >COLLECTION</li>
+                </Link>
+                <Link onClick={() => setVisible(false)}  href={'/about'} className={`${path === '/about' ? 'active' : ''} border w-full py-2 pl-8`}>
+                    <li >ABOUT</li>
+                </Link>
+                <Link onClick={() => setVisible(false)}  href={'/contact'} className={`${path === '/contact' ? 'active' : ''} border w-full py-2 pl-8`}>
+                    <li >CONTACT</li>
+                </Link>
+            </ul>
         </div>
     )
 }
