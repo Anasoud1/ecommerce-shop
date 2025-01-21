905c7a5cddd2599e1e444efca14f691a2dd2b427
anas <anasoudsassi@gmail.com>
2025-01-21 17:34:00 +0100

    adding contact page

--- src/app/(routes)/contact/page.jsx
+++ src/app/(routes)/contact/page.jsx
@@ -1,8 +1,22 @@
+import Title from '@/components/Title'
+import Image from 'next/image'
 import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='pt-10'>
+      <Title text1={'CONTACT'} text2={'US'}/>
+      <div className='flex flex-col md:flex-row gap-20 pt-16 lg:w-[80%] lg:mx-auto justify-center'>
+        <Image className='w-full flex-1' src={'/contact.jpg'} width={300} height={300} alt='contact'/>
+        <div className='flex-1'>
+          <h3 className='py-6 text-lg font-bold'>Our Store</h3>
+          <p>193 Angle Boulevard de la Résistance <br/>Marrakech, Morocco</p>
+
+          <p className='pt-8'>Tel: (+212) 655-511-414</p>
+          <p>Email: admin@gmail.com</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
